ee9a900468af9141395d5c438e1df8600aec5c1f
nedoratic <nedoratic@gmail.com>
2024-03-02 23:52:39 +0100

    dom elements added

---
+++ index.js
@@ -0,0 +1,4 @@
+// DOM elements
+const appInput = document.querySelector('.app__input__field');
+const appButton = document.querySelector('.app__input__button');
+const appOutput = document.querySelector('.app__output');
