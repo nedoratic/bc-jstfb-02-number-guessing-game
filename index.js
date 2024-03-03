66d4227ac1a3374a6d8d4678da878287760f0243
nedoratic <nedoratic@gmail.com>
2024-03-03 23:41:02 +0100

    basic game variables added

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 // DOM elements
-const appInput = document.querySelector('.app__input__field');
+const appInput = document.querySelector('.app__input__field').value;
 const appButton = document.querySelector('.app__input__button');
 const appOutput = document.querySelector('.app__output');
+
+// Game variables
+const minNum = 1;
+const maxNum = 100;
+
+let answer;
+let guess;
+
+let running = true;
+
+const generateRandomNumber = () => {
+	Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+};
